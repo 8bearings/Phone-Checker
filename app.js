d41f95b8fe4fd3873c23b525955cfa537524499c
Elliott Watson <elliottwatson0@gmail.com>
2021-07-21 01:08:42 -0400

    Gif version 1

---
+++ app.js
@@ -0,0 +1,134 @@
+
+const express = require("express");
+const app = express();
+const multer = require("multer");
+// const upload = multer({
+//   storage: multer.diskStorage({}),
+//   fileFilter: (req, file, cb) => {
+//     let ext = path.extname(file.originalname);
+//     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+//       //adding error page for filetype
+//        cb(new Error("File type is not supported"), false);
+//       return;
+//       // res.render("error.ejs") 
+//     }
+//     cb(null, true);
+//   },
+// });
+
+//MS Specific
+const axios = require("axios").default;
+const async = require("async");
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const createReadStream = require("fs").createReadStream;
+const sleep = require("util").promisify(setTimeout);
+const ComputerVisionClient =
+  require("@azure/cognitiveservices-computervision").ComputerVisionClient;
+const ApiKeyCredentials = require("@azure/ms-rest-js").ApiKeyCredentials;
+
+require("dotenv").config({ path: "./config/.env" });
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const key = process.env.MS_COMPUTER_VISION_SUBSCRIPTION_KEY;
+const endpoint = process.env.MS_COMPUTER_VISION_ENDPOINT;
+const faceEndpoint = process.env.MS_FACE_ENDPOINT;
+const subscriptionKey = process.env.MS_FACE_SUB_KEY;
+
+const computerVisionClient = new ComputerVisionClient(
+  new ApiKeyCredentials({ inHeader: { "Ocp-Apim-Subscription-Key": key } }),
+  endpoint
+);
+
+//Server Setup
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+const upload = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+      // adding error page for filetype
+       cb(new Error("File type is not supported"), false);
+      return;
+       
+      // }); 
+    }
+    cb(null, true);
+  },
+});
+
+app.post("/", upload.single("file-to-upload"), async (req, res) => {
+  try {
+    let cellphoneCount = 0;
+    // Upload image to cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+    const objectURL = result.secure_url;
+
+    // Analyze a URL image
+    console.log("Analyzing objects in image...", objectURL.split("/").pop());
+
+    const objects = (
+      await computerVisionClient.analyzeImage(objectURL, {
+        visualFeatures: ["Objects"],
+      })
+    ).objects;
+    console.log();
+
+    // Print objects bounding box and confidence
+    if (objects.length) {
+      console.log(
+        `${objects.length} object${objects.length == 1 ? "" : "s"} found:`
+      );//first object check below
+      for (const obj of objects) {
+        if (obj.object === "cell phone") {
+          cellphoneCount = cellphoneCount + 1;
+          
+          //another check below
+        }else if(obj.object === ""){
+          
+        }
+        console.log(
+          `    ${obj.object} (${obj.confidence.toFixed(
+            2
+          )}) at ${formatRectObjects(obj.rectangle)}`
+        );
+      }
+    } else {
+      console.log("No objects found.");
+    }
+
+    function formatRectObjects(rect) {
+      return (
+        `top=${rect.y}`.padEnd(10) +
+        `left=${rect.x}`.padEnd(10) +
+        `bottom=${rect.y + rect.h}`.padEnd(12) +
+        `right=${rect.x + rect.w}`.padEnd(10) +
+        `(${rect.w}x${rect.h})`
+      );
+    }
+
+    res.render("result.ejs", { count: cellphoneCount, img: objectURL });
+  } catch (err) {
+    console.log(err);
+    app.post("/", (req, res) => {
+      res.render("error.ejs");
+    }); 
+  }
+});
+
+app.listen(process.env.PORT || 8000);
+
